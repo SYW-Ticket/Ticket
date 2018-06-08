cf2458e96387d93e07e168675e14eefd068052b7
yangguoshuai <1173057075@qq.com>
2018-06-08 21:19:00 +0800

    1111

---
+++ project/src/main/java/com/ticket/UserInfo/userInfoReadDAO/IUserinfoOrder.java
@@ -0,0 +1,12 @@
+package com.ticket.UserInfo.userInfoReadDAO;
+
+/**
+ * Created by Shinelon on 2018/6/8.
+ */
+public interface IUserinfoOrder {
+
+    void selectOrder(int tel, int costState);
+
+
+
+}

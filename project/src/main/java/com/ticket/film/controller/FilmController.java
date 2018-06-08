d6a8cd7b45879218a081019006d72ded7c51b980
eterWang <648992049@qq.com>
2018-06-08 21:19:39 +0800

    1、利用消息队列，完成了热点数据的redis缓存记录 2、定时任务将电影的热点从缓存取出，更新到数据库，并将缓存清空

--- project/src/main/java/com/ticket/film/controller/FilmController.java
+++ project/src/main/java/com/ticket/film/controller/FilmController.java
@@ -1,18 +1,13 @@
 package com.ticket.film.controller;
 
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import com.ticket.film.dao.impl.FilmDao;
 import com.ticket.film.entity.FilmDetail;
-import com.ticket.film.entity.PageBean;
 import com.ticket.film.service.FilmService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -29,16 +24,6 @@ public class FilmController {
     @Resource
     private FilmService filmService;
 
-//    @RequestMapping("/LoadingFilmsByPage/{currentPage}")
-//    public String findAllFilmsLoadingByPage(@PathVariable("currentPage") int currentPage, Model model){
-//        model.addAttribute("pageBean",filmService.findAllFilmsLoadingByPage(currentPage));
-//        return "/filmList";
-//    }
-//    @RequestMapping("/willLoadByPage/{currentPage}")
-//    public String willLoadByPage(@PathVariable("currentPage") int currentPage, Model model){
-//        model.addAttribute("pageBean",filmService.filmsWillLoadByPage(currentPage));
-//        return "/filmList";
-//    }
     @RequestMapping("/LoadingByPage/{currentPage}")
     public String pageLoading(@PathVariable("currentPage") int currentPage,HttpSession session){
         PageHelper.startPage(currentPage,5);
@@ -47,6 +32,7 @@ public class FilmController {
         session.setAttribute("pageInfo",pageInfo);
         return "/filmList";
     }
+
     @RequestMapping("/willLoadByPage/{currentPage}")
     public String pageWillLoad(@PathVariable("currentPage") int currentPage,HttpSession session,Model model){
         PageHelper.startPage(currentPage,5);
@@ -62,4 +48,15 @@ public class FilmController {
         model.addAttribute("filmDetail", filmService.filmDetail(filmId));
         return "/filmDetails";
     }
+
+    //    @RequestMapping("/LoadingFilmsByPage/{currentPage}")
+//    public String findAllFilmsLoadingByPage(@PathVariable("currentPage") int currentPage, Model model){
+//        model.addAttribute("pageBean",filmService.findAllFilmsLoadingByPage(currentPage));
+//        return "/filmList";
+//    }
+//    @RequestMapping("/willLoadByPage/{currentPage}")
+//    public String willLoadByPage(@PathVariable("currentPage") int currentPage, Model model){
+//        model.addAttribute("pageBean",filmService.filmsWillLoadByPage(currentPage));
+//        return "/filmList";
+//    }
 }
